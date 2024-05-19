f7954bfe53868cf6974ca3e4bdbb1e0e3c16d264
1027603857 <38471268+1027603857@users.noreply.github.com>
2024-05-19 20:56:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 IMAGE_NAME="torch1.12.1_cuda11.3.1_cudnn8_devel:2024.5.19"
-docker build \
-	-f ./Dockerfile \
-	--progress=auto \
-	-t 1027603857/${IMAGE_TAG} .
+docker build -f ./Dockerfile --progress=auto -t 1027603857/${IMAGE_TAG} .
 docker push ${IMAGE_NAME}
