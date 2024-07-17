e2e049eaa970b784e716d3baf875114796b37754
1027603857 <38471268+1027603857@users.noreply.github.com>
2024-07-17 20:38:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-IMAGE_TAG="torch2.3.1_cuda12.1_cudnn8_devel_2024.7.17"
+IMAGE_TAG="torch2.3.0_cuda12.1_cudnn8_devel_2024.7.17"
 docker build -f ./Dockerfile --progress=auto -t "1027603857/pytorch:${IMAGE_TAG}" .
 docker push "1027603857/pytorch:${IMAGE_TAG}"
